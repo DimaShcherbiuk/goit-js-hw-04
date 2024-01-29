a21bfaf9fbb9fc1a3712a680cd028dbdea396a07
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-01-29 22:42:43 +0200

    start

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-function slugify(title) {
-
-    let titleLowerCase = title.toLowerCase();
-    let words = titleLowerCase.split(' '); 
-    let slug = words.join('-')
-    return slug;
-}
--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-function filterArray(numbers, value) {
-    let finishArrayNum = [];
-    for (let i = 0; i < numbers.length; i += 1) {
-        if (numbers[i] > value) {
-            finishArrayNum.push(numbers[i]);
-        }
-    }
-    return finishArrayNum;
-}
